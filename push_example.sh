2981083b354fa0893d0d38ad65158665d5d37cac
haozhao <haozhao@localhost.localdomain>
2017-07-05 09:10:54 +0800

    push_example

---
+++ push_example.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+read -p "input commit measure:"  val
+echo $val
+git status
+git add .
+git status
+git commit -m"$val"
+git status
+#read -p "are you sure to commit ?[Y/N]"val2
+set timeout 30
+expect <<!
+spawn git push origin master
+expect "Username"
+send "your name\r"
+expect "Password"
+send "your password\r"
+expect eof
+!
+
+
+
+
