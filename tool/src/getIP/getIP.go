eb0179d4897048025d36ab5717576220661f241f
haozhao <haozhao@localhost.localdomain>
2017-07-18 09:34:14 +0800

    1

---
+++ tool/src/getIP/getIP.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"strings"
+)
+
+var HostIp = getIp()
+var ServerName = getServerName()
+
+func main() {
+	fmt.Println(getIp())
+}
+
+func getIp() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		panic(err)
+	}
+
+	for _, addr := range addrs {
+		ip := strings.Split(addr.String(), "/")[0]
+		code := strings.Split(ip, ".")
+		switch code[0] {
+		case "10", "127":
+			continue
+		default:
+			return ip
+		}
+	}
+	panic(addrs)
+}
+
+func getServerName() string {
+	// return "etlog"
+	return getIp()
+}
--- tool/src/loghz/loghz_test.go
+++ tool/src/loghz/loghz_test.go
@@ -1,27 +1,28 @@
 package loghz
-import(
-	"testing"
+
+import (
 	"errors"
-//	"fmt"
+	"testing"
+	//	"fmt"
 )
 
-type testlog struct{
+type testlog struct {
 	A int
 	B int
 	C int
 }
 
-func TestPrintln(t  *testing.T){
+func TestPrintln(t *testing.T) {
 
-	ll:=NewLogDebug(true)
-	ll.Error(errors.New("test err"))
-	ll.Println("1","2")
-	a:=&testlog{1,2,3 }
+	ll := NewLogDebug(true)
+	ll.Error(errors.New("test err"), "message")
+	ll.Println("1", "2")
+	a := &testlog{1, 2, 3}
 	ll.Println(*a)
-	b:= map[int]int{
-		1:2,
-		2:3,
-		4:4,
+	b := map[int]int{
+		1: 2,
+		2: 3,
+		4: 4,
 	}
 	ll.Println(b)
 }
